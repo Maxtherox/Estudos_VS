6c9b5a407f7f67901e44dcef1ad7d397a24b54c7
Guilherme de jesus <gui-rock02@hotmail.com>
2021-11-24 19:49:21 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = "World!";
+            console.writeLine(a + b);
+        }
+    }
+}
